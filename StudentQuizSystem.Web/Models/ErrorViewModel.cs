612fb8b771ea8f02fcdf3a82db6d138e026047e1
Regina Angela Ramones <angela17ramones@gmail.com>
2025-11-07 22:15:13 +0800

    Delete StudentQuizSystem.Web/Models directory

--- StudentQuizSystem.Web/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace StudentQuizSystem.Web.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
